de61af0c04f1e1065fce5caa35fded90eea317cf
Shark-vil <shark_vil@mail.ru>
2022-07-29 23:41:13 +0300

    Update sbox-key-clicker.user.js

--- sbox-key-clicker.user.js
+++ sbox-key-clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         S&box key clicker
 // @namespace    https://github.com/Shark-vil
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://asset.party/~get
